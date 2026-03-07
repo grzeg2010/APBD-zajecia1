59a68a4f5198c6043dbf0cbbec5c0bd18236c104
grzeg2010 <grzeg2010@gmail.com>
2026-03-08 00:34:47 +0100

    Just having fun with C#, testing things

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+for (var i = 0; i < 50; i++)
+{
+    Console.WriteLine(i);
+}
+
+List<char> letters = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+letters.ForEach(letter => Console.WriteLine(letter));
